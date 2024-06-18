7062470d1c26569841cfdb7f86a543e6de08fc37
RAJAT SONAR <150504303+rajatsonar22@users.noreply.github.com>
2024-06-18 23:42:47 +0530

    Add files via upload

---
+++ SUM OF ELEMENTS.py
@@ -0,0 +1,29 @@
+#Sum of elements
+def sum_numbers(numbers):
+    total = 0
+    for n in numbers:
+        total += n
+    return total
+print(sum_numbers({1,2,3,4,5}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
